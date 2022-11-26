e4be682b7b150b12adbc83dcdb68be4ff31ae55b
Tim O'Brien <timobrien.inc@gmail.com>
2022-11-26 16:03:24 -0500

    added backend user and bookmark routes with auth middleware, added supporting npm packages

--- server/server.js
+++ server/server.js
@@ -1,5 +1,11 @@
 import express from 'express'
 import devBundle from './devBundle'
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+
+connectDB()
 
 const app = express()
 devBundle.compile(app)
@@ -8,11 +14,19 @@ import path from 'path'
 const CURRENT_WORKING_DIR = process.cwd()
 app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 import template from './../template'
+
+app.use('/user', require('./routes/userRoutes'))
+app.use('/bookmark', require('./routes/bookmarkRoutes'))
 
 app.get('*', (req, res) => {
   res.status(200).send(template())
 })
+
+app.use(errorHandler)
 
 let port = process.env.PORT || 3000
 app.listen(port, (err) => {
